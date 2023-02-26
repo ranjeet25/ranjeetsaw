474cd7abe66a249910e876fba0fc1993381fd5db
Ranjeet Saw <sawranjeet550@gmail.com>
2023-02-26 15:49:34 +0530

    modified Heading

--- app/Heading.js
+++ app/Heading.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Heading(props) {
   return (
-    <div className="flex w-[100%] justify-center items-center bg-base-100 pt-4">
+    <div className="flex w-[100%] justify-center items-center bg-transparent pt-4">
       <div className="w-full bg-pink-600 h-[2px] basis-5/6"></div>
 
       <div className="w-64 text-center font-bold text-lg bg-base-300 rounded-full px-3 py-1">
